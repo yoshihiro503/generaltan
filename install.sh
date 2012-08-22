47cb6e20edeb4c35f9fd6708066be5636bda696e
yoshihiro503 <y.imai@ocaml.jp>
2012-08-22 17:27:50 +0900

    install script for splash image of ProofGeneral

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+PROOF_GENERAL=YOUR_PROOFGENERAL_DIR
+mv -i ${PROOF_GENERAL}/images/ProofGeneral.jpg ${PROOF_GENERAL}/images/ProofGeneral.orig.jpg
+cp Generaltan-189x247.jpg ${PROOF_GENERAL}/images/ProofGeneral.jpg
+
